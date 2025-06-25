e895c58bbb5d2d7ea39026dfd82f5d63d26ca99e
SAGAR-RAJ1 <157152445+SAGAR-RAJ1@users.noreply.github.com>
2025-06-25 14:09:14 +0530

    Created Reducers

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,1 +1,37 @@
 //reducers
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const intitalstate = {
+  todos: [{ id: "abc", task: "demo-task", isDone: false }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  intitalstate,
+  reducers: {
+    //action state
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        task: action.payload,
+        isDone: false,
+      };
+      state.todo.push(newTodo);
+    },
+    deleteTodo: (state, action) => {
+      // action.payload
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    marksAsDone: (state, action) => {
+      //action.payload
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.isDone = true;
+        }
+      });
+    },
+  },
+});
+
+export default todoSlice.reducer;
